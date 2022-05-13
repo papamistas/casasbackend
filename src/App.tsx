a9b9f1266e604da753d45369527a7bb24abefc85
Led <leoneldomingos@sapo.pt>
2022-05-13 02:32:58 +0100

    authentication apitoken not working

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,7 @@ function App() {
       ReadyPage={ReadyPage}
       catchAll={<ErrorComponent />}
       routerProvider={routerProvider}
-      dataProvider={dataProvider("http://192.168.192.4")}
+      dataProvider={dataProvider("http://192.168.192.4/api")}
       accessControlProvider={{
         can: async ({ resource, action }) => {
           const enforcer = await newEnforcer(model, adapter);
